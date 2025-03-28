762dd8634820c56f760a32b4ed367256a5f3b89c
WhithoutTraceHD <gianni.coccioli@outlook.de>
2025-03-28 09:41:40 +0100

    vite angepasst

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,10 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import VitePluginRewriteAll from 'vite-plugin-rewrite-all'; // 🔄 korrekt!
 
 export default defineConfig({
-  plugins: [react()],
+  base: '/',
+  plugins: [react(), VitePluginRewriteAll()],
   server: {
-    port: 5173
+    port: 5173,
   },
-  base: '/'
-})
+});
