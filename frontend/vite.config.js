f2d53fca6b3ac40e4dff5126b7dcb2b4f2fc6d40
WhithoutTraceHD <gianni.coccioli@outlook.de>
2025-03-28 09:45:41 +0100

    _redirects in dist ordner

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,10 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import VitePluginRewriteAll from 'vite-plugin-rewrite-all'; // 🔄 korrekt!
 
 export default defineConfig({
   base: '/',
-  plugins: [react(), VitePluginRewriteAll()],
+  plugins: [react()],
   server: {
     port: 5173,
   },
